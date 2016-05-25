e023bc7a697b01bb181b7ad990182c791ca485fb
harrij15 <harrij15@rpi.edu>
2016-05-24 22:56:16 -0400

    Actually add files this time

---
+++ DriverMode/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  DriverMode
+//
+//  Created by Jon Harris on 5/24/16.
+//  Copyright © 2016 harrij15. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ DriverModeTests/DriverModeTests.swift
@@ -0,0 +1,36 @@
+//
+//  DriverModeTests.swift
+//  DriverModeTests
+//
+//  Created by Jon Harris on 5/24/16.
+//  Copyright © 2016 harrij15. All rights reserved.
+//
+
+import XCTest
+@testable import DriverMode
+
+class DriverModeTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
